19f7c6b5121cc13e733925690075b2112b0c3bf6
Omm Prakash Sahu <140820137+ommprakashsahu1234@users.noreply.github.com>
2024-08-13 20:50:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function fixNav() {
         nav.classList.remove('active')
     }
 }
+
+
+
+//Remove the below code when adjusted for Android View 
+
+function isDesktop() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobile = /iphone|ipad|android|mobile|tablet|phone/i.test(userAgent);
+    return !isMobile;
+  }
+
+  // Check if the device is not a desktop
+  if (!isDesktop()) {
+    alert("For Best View, Open in Desktop");
+  }
